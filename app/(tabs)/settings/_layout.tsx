99247fec9f996addaa81cc9314def89f95a2df36
Braian Emmanuel Carabajal <braiancarabajal@gmail.com>
2024-03-19 11:11:54 -0300

    Configured Settings stack

---
+++ app/(tabs)/settings/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+
+import Colors from "@/constants/Colors";
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Settings",
+          headerLargeTitle: true,
+          headerShadowVisible: false,
+          headerStyle: {
+            backgroundColor: Colors.background,
+          },
+          headerSearchBarOptions: {
+            placeholder: "Search",
+          },
+        }}
+      />
+    </Stack>
+  );
+};
+export default Layout;
